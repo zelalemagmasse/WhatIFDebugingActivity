8de99138c43384407d3022807ecabc537ae8579e
zelalem <zelalem.agmasse@gmail.com>
2018-06-01 12:58:33 -0400

    initial commit

---
+++ src/com/company/StudentRecordReader.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class StudentRecordReader {
+
+    public static void main(String[] args) {
+        String fname, lname;
+        int gpa;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print( "Student's First Name? " );
+        fname = keyboard.next();
+
+        System.out.print( "Student's Last Name? " );
+        lname = keyboard.next();
+
+        System.out.print( "Student's GPA? " );
+        gpa = keyboard.nextInt();
+
+        System.out.println();
+        System.out.println("Student Name :" + fname + " " + lname);
+        System.out.println("Student GPA :" + gpa);
+        if (gpa >= 3)
+        {
+            System.out.println("This student is in good standing.");
+        } else if (gpa == 1)
+        {
+            System.out.println("This student is on academic probation.");
+        } else
+        {
+            System.out.println("This student has been expelled.");
+        }
+    }
+}
+
